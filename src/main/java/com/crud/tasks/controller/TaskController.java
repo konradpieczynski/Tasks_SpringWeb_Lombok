25d5fee332a26ce352e7cc97891a28d1c47f8fb6
Konrad Pieczynski <konrad.pieczynski@gmail.com>
2023-04-18 20:03:10 +0200

    19.2 Task

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -1,22 +1,35 @@
 package com.crud.tasks.controller;
 
+import com.crud.tasks.domain.Task;
 import com.crud.tasks.domain.TaskDto;
+import com.crud.tasks.mapper.TaskMapper;
+import com.crud.tasks.service.DbService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/v1/tasks")
+@RequiredArgsConstructor
 public class TaskController {
+    private final DbService service;
+    private final TaskMapper taskMapper;
+
     //@RequestMapping(method = RequestMethod.GET, value = "")
     @GetMapping
     public List<TaskDto> getTasks() {
-        return new ArrayList<>();
+        List<Task> tasks = service.getAllTasks();
+        return taskMapper.mapToTaskDtoList(tasks);
     }
     @GetMapping(value = "{taskId}")
-    public TaskDto getTask(@PathVariable Long taskId) {
-        return new TaskDto(1L, "test title", "test_content");
+    public ResponseEntity<TaskDto> getTask(@PathVariable Long taskId) {
+        Optional<Task> task = service.getTask(taskId);
+        return task.map(value -> ResponseEntity.ok().body(taskMapper.mapToTaskDto(value))).orElseGet(() -> ResponseEntity.notFound().build());
     }
     @DeleteMapping
     public void deleteTask(Long taskId) {
